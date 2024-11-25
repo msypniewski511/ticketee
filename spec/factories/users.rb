f158a606cca4b6166bb7282126b356dbfa8d7979
Maciej <msypniewski511@gmail.com>
2024-11-25 19:27:17 +0000

    Added admin flag to User model, and seeded the first admin user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |i| "text#{i}@example.com" }
     password { "password" }
+
+    trait :admin do
+      admin { true }
+    end
   end
 end
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.feature "Users can create new project" do
   before do
+    login_as(FactoryBot.create(:user, :admin))
     visit "/"
     click_link "New Project"
   end
