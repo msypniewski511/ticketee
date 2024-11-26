a7a55d1fd2d480631dcb165da9c811850ccee552
Maciej <msypniewski511@gmail.com>
2024-11-26 05:46:51 +0000

    dd admin namespace with application controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,5 +63,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   # 
   config.include Warden::Test::Helpers, type: :feature
+  config.include Warden::Test::Helpers, type: :request
   config.after(type: :feature) { Warden.test_reset! }
 end
---
+++ spec/requests/admin/application_request_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "Admin::Applications", type: :request do
+
+  describe "GET /" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:admin) { FactoryBot.create(:user, :admin) }
+
+    context "when sign in as a user" do
+      before do
+        login_as(user)
+      end
+
+      it "redirect away" do
+        get "/admin"
+        expect(response).to have_http_status(302)
+        follow_redirect!
+        expect(response.body).to include "You must be an admin to do that."
+      end
+    end
+
+    context "when signed in as admin" do
+      before do
+        login_as(admin)
+      end
+
+      it "lets the admin in" do
+        get "/admin"
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+end
