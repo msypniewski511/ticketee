8a401fc879f47cbbf42a24b6174a40178adce500
Maciej <msypniewski511@gmail.com>
2024-12-10 17:41:46 +0000

    Add button so users can toggle watching on a ticket

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,6 @@
 module TicketsHelper
+  def toggle_watching_button(ticket)
+    text = ticket.watchers.include?(current_user) ? "Unwatch" : "Watch"
+    link_to text, watch_project_ticket_path(ticket.project, ticket), class: text.parameterize, method: :patch
+  end
 end
